e4320a05fbb9e5b731e3ae7a3756398917c73b17
ethmth <ethan@smaze.net>
2023-05-29 12:26:23 -0400

    Added fdisk partitioning

--- arch-install.sh
+++ arch-install.sh
@@ -5,10 +5,34 @@
 # git clone https://github.com/ethmth/install-scripts.git
 # Run this script
 
+# Install necessary packages
 pacman -S vim fzf
 
+# Font, Time
 setfont ter-122b
 timedatectl
+read -p "Is the timedatectl correct (zone does not matter, just clock)?" userInput
 
-disk=$(fdisk -l | grep "Disk /dev/" | grep -v "loop" | fzf | awk -F'/' '{print $3}' | awk -F':' '{print $1}')
+# Disk partititioning
+disk=$(fdisk -l | grep "Disk /dev/" | grep -v "loop" | fzf --prompt="Select disk for BOOT and ROOT partitions" | awk -F'/' '{print $3}' | awk -F':' '{print $1}')
 disk="/dev/$disk"
+# to create the partitions programatically (rather than manually)
+# we're going to simulate the manual input to fdisk
+# The sed script strips off all the comments so that we can 
+# document what we're doing in-line with the actual commands
+# Note that a blank line (commented as "defualt" will send a empty
+# line terminated with a newline to take the fdisk default.
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${disk}
+  g # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +1G # 1 GB boot parttion
+  n # new partition
+  p # primary partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  w # write the partition table
+EOF
