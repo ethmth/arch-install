9526208e2149233b64b07632ee108d9f099eaa89
ethmth <ethan@smaze.net>
2023-05-29 11:58:02 -0400

    Get disks

--- arch-install.sh
+++ arch-install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Connect to internet
 # pacman -S git
 # git clone https://github.com/ethmth/install-scripts.git
+# Run this script
 
+pacman -S vim fzf
+
+setfont ter-122b
+timedatectl
+
+disk=$(fdisk -l | grep "Disk /dev/" | grep -v "loop" | fzf | awk -F'/' '{print $3}' | awk -F':' '{print $1}')
+disk="/dev/$disk"
