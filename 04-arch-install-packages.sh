7f6465c68e73afc1e8cd48d1e74ca91e564c15e9
ethmth <ethan@smaze.net>
2023-05-29 22:59:20 -0400

    Added more grub changes

--- 04-arch-install-packages.sh
+++ 04-arch-install-packages.sh
@@ -176,4 +176,10 @@ sudo usermod -aG network,libvirt,kvm,input,docker,vboxusers,wireshark,autologin 
 # Update grub with new config and kernels
 sudo sh -c 'echo "GRUB_SAVEDEFAULT=true" >> /etc/default/grub'
 sudo sh -c 'echo "GRUB_DISABLE_SUBMENU=y" >> /etc/default/grub'
+sudo sed -i 's/GRUB_DEFAULT=0/#&\nGRUB_DEFAULT=saved/' /etc/default/grub
+sudo sed -i 's/GRUB_TIMEOUT=5/#&\nGRUB_TIMEOUT=3/' /etc/default/grub
+if (( NVIDIA )); then
+    current_arguments=$(grep "^GRUB_CMDLINE_LINUX_DEFAULT" /etc/default/grub | sed 's/GRUB_CMDLINE_LINUX_DEFAULT=//' | tr -d '"')
+    sudo sed -i "s/^\(GRUB_CMDLINE_LINUX_DEFAULT=\).*/\1\"ibt=off $current_arguments\"/" /etc/default/grub
+fi
 sudo grub-mkconfig -o /boot/grub/grub.cfg
