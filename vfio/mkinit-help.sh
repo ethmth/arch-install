2696164d2c8bd72381f07094c72e0e212b81a381
ethmth <ethan@smaze.net>
2023-05-31 14:47:20 -0400

    Check whether after/before word is in file

--- vfio/mkinit-help.sh
+++ vfio/mkinit-help.sh
@@ -8,16 +8,15 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-if [ "$1" == "add-hook" ]; then 
-  printf ""
-elif [ "$1" == "ls-hook" ]; then 
-  printf ""
+EDIT_HOOKS=0
+EDIT_MODULES=0
+if [ "$1" == "add-hook" ]; then
+    EDIT_HOOKS=1
 elif [ "$1" == "add-module" ]; then
-  printf ""
+    EDIT_MODULES=1
 else
 	printf "Options \n"
 	printf	"\t add-hook\n"
-	printf	"\t ls-hook\n"
 	printf	"\t add-module\n"
     exit 1
 fi
@@ -28,11 +27,12 @@ AFTER=0
 BEFORE=0
 after_word=""
 before_word=""
+target_word=""
 
 while getopts "ab" opt; do
   case $opt in
     a)
-      echo "Option A is selected"
+      #echo "Option A is selected"
       # Do something for option A
       AFTER=1
       shift
@@ -40,7 +40,7 @@ while getopts "ab" opt; do
       shift
       ;;
     b)
-      echo "Option B is selected"
+      #echo "Option B is selected"
       # Do something for option B
       BEFORE=1
       shift
@@ -49,7 +49,6 @@ while getopts "ab" opt; do
       ;;
     \?)
       echo "Invalid option: -$OPTARG"
-      usage
       exit 1
       ;;
   esac
@@ -61,27 +60,52 @@ if (( AFTER && BEFORE )); then
 fi
 
 if (( AFTER )); then
-    echo "After $after_word"
+    target_word=$after_word
 fi
 if (( BEFORE )); then
-    echo "Before $before_word"
+    target_word=$before_word
 fi
 
-hooks=("$@")  # Add the hooks you want to check and add here
+if ! grep -q "^HOOKS=.*$target_word" "$conf_file"; then
+        echo "The hook $target_word is not present in $conf_file."
+        exit 1
+fi
 
-#echo "BEFORE: $(grep -q "^HOOKS=.*base" "$conf_file")"
-current_hooks=$(grep "^HOOKS=" "$conf_file" | sed -e 's/^HOOKS=//')
-printf "Previous Hooks:\n $current_hooks\n"
-#grep -q "^HOOKS=.*base" "$conf_file"
-
+hooks_string="$@"
+#hooks=("$hooks_string")
+hooks=("$@")
+#hooks=("$@")
+#hooks=("$hooks_string")
 #echo "$hooks"
+#echo "$hooks_string"
+
+current_hooks=$(grep "^HOOKS=" "$conf_file" | sed -e 's/^HOOKS=//')
+#printf "Previous Hooks:\n$current_hooks\n"
+
+new_hooks="$current_hooks"
 
 # Check if the hooks are already present in the file
 for hook in "${hooks[@]}"; do
-    if grep -q "^HOOKS=.*$hook" "$conf_file"; then
-        echo "The hook $hook is already present in $conf_file."
-    else
-        echo "Adding the hook $hook to $conf_file."
-        sed -i "/^HOOKS=/ s/\(^\(HOOKS=.*\)\)\$/\1 $hook/" "$conf_file"
-    fi
+    #if grep -q "^HOOKS=.*$hook" "$current_hooks"; then
+    #    echo "The hook $hook is already present in $current_hooks."
+    #else
+    #    echo "Adding the hook $hook to $current_hooks."
+    #    sed -i "/^HOOKS=/ s/\(^\(HOOKS=.*\)\)\$/\1 $hook/" "$current_hooks"
+    #fi
+    new_hooks=${new_hooks//"$hook "/}
+    new_hooks=${new_hooks//"$hook)"}
+    new_hooks=${new_hooks/". $hook"/}
+    #echo "$hook"
 done
+#echo "$new_hooks"
+#index=$(echo $new_hooks | grep -o "\b$after\b" | wc -w)
+#index=$((index + 1))
+
+#new_hooks="${current_hooks:0:$index} $hooks ${current_hooks:$index}"
+#index=$(echo $original_hooks | awk -v after="$after" '{for (i=1; i<=NF; i++) if ($i == after) {print i; break}}')
+#new_hooks="${new_hooks:0:$index} $hooks ${new_hooks:$index}"
+
+new_string=$(echo "$new_string" | sed "s/$after_word/$after_word $hooks_string/")
+
+echo "Original hooks: $current_hooks"
+echo "New hooks: $new_hooks"
