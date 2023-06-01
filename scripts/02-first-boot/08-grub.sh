b70510c8aad5fb41c996dd8d9c759aa95cdeec70
ethmth <ethan@smaze.net>
2023-05-31 21:51:20 -0400

    Finished up scripts in 02-first-boot

--- scripts/02-first-boot/08-grub.sh
+++ scripts/02-first-boot/08-grub.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# Load Config Values
+if ! [[ $EUID -ne 0 ]]; then
+	echo "This script should not be run with root/sudo privileges."
+	exit 1
+fi
+
 CUR_USER=$(whoami)
-source /home/$CUR_USER/install-scripts/values.conf
+source /home/$CUR_USER/arch-install/config/system.conf
 
-
-# Update grub with new config and kernels
-sudo sh -c 'echo "GRUB_SAVEDEFAULT=true" >> /etc/default/grub'
-sudo sh -c 'echo "GRUB_DISABLE_SUBMENU=y" >> /etc/default/grub'
-sudo sed -i 's/GRUB_DEFAULT=0/#&\nGRUB_DEFAULT=saved/' /etc/default/grub
-sudo sed -i 's/GRUB_TIMEOUT=5/#&\nGRUB_TIMEOUT=3/' /etc/default/grub
+# sudo sh -c 'echo "GRUB_SAVEDEFAULT=true" >> /etc/default/grub'
+sudo -k bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/default/grub "GRUB_SAVEDEFAULT=true"
+# sudo sh -c 'echo "GRUB_DISABLE_SUBMENU=y" >> /etc/default/grub'
+sudo bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/default/grub "GRUB_DISABLE_SUBMENU=y"
+# sudo sed -i 's/GRUB_DEFAULT=0/#&\nGRUB_DEFAULT=saved/' /etc/default/grub
+sudo bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/default/grub "GRUB_DEFAULT=saved"
+# sudo sed -i 's/GRUB_TIMEOUT=5/#&\nGRUB_TIMEOUT=3/' /etc/default/grub
+sudo bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/default/grub "GRUB_TIMEOUT=3"
 if (( NVIDIA )); then
-    current_arguments=$(grep "^GRUB_CMDLINE_LINUX_DEFAULT" /etc/default/grub | sed 's/GRUB_CMDLINE_LINUX_DEFAULT=//' | tr -d '"')
-    sudo sed -i "s/^\(GRUB_CMDLINE_LINUX_DEFAULT=\).*/\1\"ibt=off $current_arguments\"/" /etc/default/grub
+    # current_arguments=$(grep "^GRUB_CMDLINE_LINUX_DEFAULT" /etc/default/grub | sed 's/GRUB_CMDLINE_LINUX_DEFAULT=//' | tr -d '"')
+    # sudo sed -i "s/^\(GRUB_CMDLINE_LINUX_DEFAULT=\).*/\1\"ibt=off $current_arguments\"/" /etc/default/grub
+    sudo bash /home/$CUR_USER/arch-install/util/kernel/grub-add.sh "ibt=off"
+    echo "NO NEED TO DO WHATEVER THIS SCRIPT SAYS ^"
 fi
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Verify that the grub config /etc/default/grub is correct."
+echo "Reboot. After (if needed) configure other disk drives using the scripts found in /home/$CUR_USER/arch-install/util/disk"
+echo "Then, run '~/arch-install/scripts/03-setup/01-packages-opt.sh'"
