913d96022d14f172315bec3e24404b461fa18ea0
ethmth <ethan@smaze.net>
2023-06-10 10:56:56 -0400

    Updated Windows Script Mounting

--- scripts/vm/02-resources-disk.sh
+++ scripts/vm/02-resources-disk.sh
@@ -7,7 +7,10 @@ fi
 
 CUR_USER=$(whoami)
 
-mkisofs -o /home/$CUR_USER/arch-install/files/resources.iso /home/$CUR_USER/arch-install/files/vm-resources
+mkisofs -o /home/$CUR_USER/arch-install/files/resources.iso -J -R /home/$CUR_USER/arch-install/files/vm-resources
+
+chmod g+rw /home/$CUR_USER/arch-install/files/resources.iso
+chmod o+rw /home/$CUR_USER/arch-install/files/resources.iso
 
 echo "If the iso file at /home/$CUR_USER/arch-install/files/resources.iso was created successfully,"
 echo "Run ./03-network-default.sh to define the default network."
