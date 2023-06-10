ea5273d7562c9d6bae611c68ec7ce8c51251341b
ethmth <ethan@smaze.net>
2023-06-10 12:42:33 -0400

    Added chocolatey package

---
+++ files/vm-resources/windows/02-install-packages.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "packages=firefox"
+set "packages=%packages% vlc"
+set "packages=%packages% notepadplusplus"
+set "packages=%packages% librewolf"
+set "packages=%packages% steam"
+set "packages=%packages% ea-app"
+set "packages=%packages% curl"
+set "packages=%packages% wget"
+set "packages=%packages% git"
+set "packages=%packages% python"
+set "packages=%packages% 7zip"
+set "packages=%packages% epicgameslauncher"
+set "packages=%packages% chromium"
+set "packages=%packages% tor-browser"
+set "packages=%packages% prismlauncher"
+set "packages=%packages% qbittorrent"
+set "packages=%packages% discord"
+set "packages=%packages% audacity"
+set "packages=%packages% obs-studio"
+set "packages=%packages% vscode"
+
+
+set "package_list="
+
+for /f "delims=" %%p in ('echo %packages%') do (
+    set "package_list=!package_list!%%p "
+)
+
+set "package_list=!package_list:~0,-1!"
+
+echo Packages to be installed: !package_list!
+
+choco install !package_list! -y
