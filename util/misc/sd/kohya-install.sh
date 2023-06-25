66a07ef1b986985c6d4649e12c745bc48bdb22ee
ethmth <ethan@smaze.net>
2023-06-25 17:13:53 -0400

    Finished kohya installation script

---
+++ util/misc/sd/kohya-install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if ! [[ $EUID -ne 0 ]]; then
+        echo "This script should not be run with root/sudo privileges."
+        exit 1
+fi
+
+CUR_USER=$(whoami)
+
+LOC=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | fzf --prompt="Select your desired kohya installation location")
+
+if ([ "$LOC" == "" ] || [ "$LOC" == "Cancel" ]); then
+    echo "Nothing was selected"
+    echo "Run this script again with target drive mounted."
+    exit 1
+fi
+
+if [ "$LOC" == "/" ]; then
+    LOC="/home/$CUR_USER"
+fi
+
+if ! [ -d "$LOC" ]; then
+    echo "Your location is not available. Is the disk mounted? Do you have access?"
+	exit 1
+fi
+
+LOC="$LOC/programs"
+mkdir -p $LOC
+
+git clone https://github.com/bmaltais/kohya_ss.git $LOC/kohya_ss
+
+cd $LOC/kohya_ss
+chmod +x ./setup.sh
+./setup.sh
+
+read -p "Input username for webui (will be echoed): " username
+read -p "Input password for webui (will be echoed): " password
+
+echo "#!/bin/bash" > $LOC/kohya_ss/run.sh
+echo "bash $LOC/kohya_ss/gui.sh --headless --listen 0.0.0.0 --server_port 7861  --username $username --password $password" >> $LOC/kohya_ss/run.sh
+chmod +x $LOC/kohya_ss/run.sh
+
+echo "Installation complete. To run:"
+echo "bash $LOC/kohya_ss/run.sh"
