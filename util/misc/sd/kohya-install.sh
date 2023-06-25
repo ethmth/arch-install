04b9946708230ea42ac207505b6a723a805ccec4
ethmth <ethan@smaze.net>
2023-06-25 19:49:41 -0400

    Updated kohya install for docker-compose and models

--- util/misc/sd/kohya-install.sh
+++ util/misc/sd/kohya-install.sh
@@ -24,34 +24,46 @@ if ! [ -d "$LOC" ]; then
 	exit 1
 fi
 
+DISK=$LOC
 LOC="$LOC/programs"
 mkdir -p $LOC
 
-# git clone https://github.com/bmaltais/kohya_ss.git $LOC/kohya_ss
+git clone https://github.com/bmaltais/kohya_ss.git $LOC/kohya_ss
 
 cd $LOC/kohya_ss
-# chmod +x ./setup.sh
-# ./setup.sh
 
 read -p "Input username for webui (will be echoed): " username
 read -p "Input password for webui (will be echoed): " password
 
-sed -i "s/127.0.0.1/0.0.0.0/g" docker-compose.yaml
-sed -i "s|CLI_ARGS: \"\"|CLI_ARGS: \"--headless --listen 0.0.0.0 --server_port 7861  --username $username --password $password\"|g" docker-compose.yaml
+sed -i "s/127.0.0.1:7860/0.0.0.0:7861/g" docker-compose.yaml
+sed -i "s|CLI_ARGS: \"\"|CLI_ARGS: \"--headless --listen 0.0.0.0 --username $username --password $password\"|g" docker-compose.yaml
+
+echo "#!/bin/bash" > $LOC/kohya_ss/run.sh
+echo "docker compose --project-directory=$LOC/kohya_ss run --service-ports kohya-ss-gui" >> $LOC/kohya_ss/run.sh
+chmod +x $LOC/kohya_ss/run.sh
+
+file_path="docker-compose.yaml" 
+temp_file="docker-compose.temp"
+
+mkdir -p $DISK/sd
+chmod -R o+rwx $DISK/sd
+
+while IFS= read -r line; do
+    line_without_cr=$(echo "$line" | tr -d '\r')
+    echo "$line_without_cr" >> "$temp_file"
+
+    if [[ $line == *"volumes"* ]]; then
+        echo "      - $DISK/sd:/sd" >> "$temp_file"
+        if [ -d "$LOC/stable-diffusion-webui/models" ]; then
+            chmod -R o+rwx $LOC/stable-diffusion-webui/models
+            echo "      - $LOC/stable-diffusion-webui/models:/models" >> "$temp_file"
+        fi
+    fi
+done < "$file_path"
+
+mv "$temp_file" "$file_path"
 
 docker compose build
 
-# echo "#!/bin/bash" > $LOC/kohya_ss/run.sh
-# echo "bash $LOC/kohya_ss/gui.sh --headless --listen 0.0.0.0 --server_port 7861  --username $username --password $password" >> $LOC/kohya_ss/run.sh
-# chmod +x $LOC/kohya_ss/run.sh
-
-# echo "Now, you will go through the accelerate config setup process"
-# echo "Default values are fine. fp16 mixed precision."
-# read -p "Press ENTER to continue" userInput
-
-# source venv/bin/activate
-# # accelerate config --config_file config_files/accelerate/default_config.yaml
-# accelerate config
-
-# echo "Installation complete. To run:"
-# echo "bash $LOC/kohya_ss/run.sh"
+echo "Docker version installed."
+echo "Run ./run.sh to start. Place files in $DISK/sd"
