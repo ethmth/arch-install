236764e706d8626426e7134f8800eb23933fed4c
ethmth <ethan@smaze.net>
2023-06-27 20:06:27 -0400

    Added vscode to macos installers

--- files/vm-resources/mac/05-brew-packages-opt.sh
+++ files/vm-resources/mac/05-brew-packages-opt.sh
@@ -8,6 +8,7 @@ chromium
 flutter
 cocoapods
 android-studio
+visual-studio-code
 "
 
 packages=${packages//$'\n'/ }
@@ -16,3 +17,4 @@ packages=$(echo "$packages" | tr -s ' ' | sed -e 's/^[[:space:]]*//' -e 's/[[:sp
 brew install $packages
 
 echo "Package installation complete"
+echo "Install XCode from the App Store"
