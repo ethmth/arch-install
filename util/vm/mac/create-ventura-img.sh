b169dc6fb13c2a98f83d6c235bc0f19a71d61b7c
ethmth <ethan@smaze.net>
2023-06-28 10:06:42 -0400

    Added create-ventura-img script for MacOS

--- util/vm/mac/create-ventura-img.sh
+++ util/vm/mac/create-ventura-img.sh
@@ -6,3 +6,22 @@ if ! [[ $EUID -ne 0 ]]; then
 fi
 
 CUR_USER=$(whoami)
+
+
+if [ -e "/home/$CUR_USER/vm/os/macos-Ventura.img" ]; then
+    echo "/home/$CUR_USER/vm/os/macos-Ventura.img already exists"
+    exit 1
+fi
+
+if ! [ -e "/home/$CUR_USER/vm/osx/OSX-KVM/Ventura.dmg" ]; then
+    echo "/home/$CUR_USER/vm/osx/OSX-KVM/Ventura.dmg does not exist"
+    echo "Be sure you've run through the Mac-side setup."
+    exit 1
+fi
+
+dmg2img -i /home/$CUR_USER/vm/osx/OSX-KVM/Ventura.dmg /home/$CUR_USER/vm/os/macos-Ventura.img
+
+echo "If dmg2img says dmg is corrupted, try just renameing the dmg file to an img"
+# mv /home/$CUR_USER/vm/osx/OSX-KVM/Ventura.dmg /home/$CUR_USER/vm/os/macos-Ventura.img
+
+echo "/home/$CUR_USER/vm/os/macos-Ventura.img created"
