a44a796873dd7746029c591af0abb2767fbdb7a9
ethmth <ethan@smaze.net>
2023-07-02 13:22:34 -0400

    Switched to /mnt/share instead of ~/share

--- files/vm-resources/nixos/05-share-setup.sh
+++ files/vm-resources/nixos/05-share-setup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-if ! [[ $EUID -ne 0 ]]; then
-	echo "This script should not be run with root/sudo privileges."
+if [[ $EUID -ne 0 ]]; then
+	echo "This script should be run with root/sudo privileges."
 	exit 1
 fi
 
-CUR_USER=$(whoami)
-sudo -k mkdir -p /sharepoint
-mkdir -p /home/$CUR_USER/share
-sudo mount -t 9p -o trans=virtio /sharepoint /home/$CUR_USER/share/
+mkdir -p /sharepoint
+mkdir -p /mnt/share
+mount -t 9p -o trans=virtio /sharepoint /mnt/share/
 
-string_to_echo=$(echo "  fileSystems.\"/home/$CUR_USER/share\" = {
+string_to_echo=$(echo "  fileSystems.\"/mnt/share\" = {
     device = \"/sharepoint\";
     fsType = \"9p\";
     options = [ \"trans=virtio\" ];
