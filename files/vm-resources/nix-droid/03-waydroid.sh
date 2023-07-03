60be51f69e8c180fd4c56f539f2df332fdf010ec
ethmth <ethan@smaze.net>
2023-07-03 09:52:16 -0400

    Added todo notes

--- files/vm-resources/nix-droid/03-waydroid.sh
+++ files/vm-resources/nix-droid/03-waydroid.sh
@@ -9,3 +9,10 @@ waydroid session start
 
 waydroid prop set persist.waydroid.width 768
 waydroid prop set persist.waydroid.height 1280
+
+
+# echo "MUST ENABLE OPENGL AND VIRTIO ACCELERATION FOR THE VM"
+# socat tcp-listen:5555,fork,reuseaddr tcp:192.168.240.112:5555
+
+
+# TODO - Start weston automatically, start waydroid session after weston's started, start socat command, get it working headlessly, setup libhoudini
