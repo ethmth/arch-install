7d8088ac545ef85d198e3ad891d1ab038002cf4d
ethmth <ethan@smaze.net>
2023-07-04 12:50:32 -0400

    Added pingphone script

--- files/vm-resources/ubuntu/06-user-configuration.sh
+++ files/vm-resources/ubuntu/06-user-configuration.sh
@@ -4,6 +4,21 @@ if ! [[ $EUID -ne 0 ]]; then
 	echo "This script must not be run with root/sudo privileges."
 	exit 1
 fi
+CUR_USER=$(whoami)
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.desktop.session idle-delay 0
+
+echo "Set gsettings"
+
+if ! [ -f "$SCRIPT_DIR/pingphone.desktop" ]; then
+    echo "$SCRIPT_DIR/pingphone.desktop not found"
+    exit 0
+fi
+
+mkdir -p /home/$CUR_USER/.config/autostart
+
+cp "$SCRIPT_DIR/pingphone.desktop" /home/$CUR_USER/.config/autostart/pingphone.desktop
