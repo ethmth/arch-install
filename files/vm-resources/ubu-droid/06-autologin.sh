b52d5fc9d9513accb132190cd9d2838e7d3f2aa1
ethmth <ethan@smaze.net>
2023-07-06 21:28:24 -0400

    Added checks to waydroid-start

--- files/vm-resources/ubu-droid/06-autologin.sh
+++ files/vm-resources/ubu-droid/06-autologin.sh
@@ -1,9 +1,21 @@
- #!/bin/bash
+#!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-        echo "This script should be run with root/sudo privileges."
-        exit 1
+    echo "This script should be run with root/sudo privileges."
+    exit 1
 fi
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+if ! [ -f "$SCRIPT_DIR/waydroid-start" ]; then
+    echo "$SCRIPT_DIR/waydroid-start not found"
+    exit 1
+fi
+
+mkdir -p /home/android/bin
+cp "$SCRIPT_DIR/waydroid-start" /home/android/bin/waydroid-start
+chmod +x /home/android/bin/waydroid-start
 
 mkdir -p /etc/systemd/system/getty@tty1.service.d
  
@@ -17,7 +29,6 @@ systemctl enable getty@tty1.service
 
 echo 'if [ "$WAYLAND_DISPLAY" == "" ]; then' >> /home/android/.bashrc
 echo 'weston' >> /home/android/.bashrc
-echo 'waydroid session start' >> /home/android/.bashrc
 echo 'fi' >> /home/android/.bashrc
 
 echo "Reboot."
