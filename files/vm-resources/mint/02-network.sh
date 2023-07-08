af55546e6fec052e3299c2ac93ba36649baa6c70
ethmth <ethan@smaze.net>
2023-07-07 20:28:51 -0400

    Fixed bugs in mint scripts'

--- files/vm-resources/mint/02-network.sh
+++ files/vm-resources/mint/02-network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
+if ! [[ $EUID -ne 0 ]]; then
 	echo "This script must not be run with root/sudo privileges."
 	exit 1
 fi
@@ -26,8 +26,9 @@ nmcli connection modify "$CONNECTION1" ipv4.dns 10.152.152.10
 nmcli connection down "$CONNECTION1"
 nmcli connection up "$CONNECTION1"
 
-nmcli connection modify "$CONNECTION2" ipv4.method shared subnet 10.153.153.0/24
+nmcli connection modify "$CONNECTION2" ipv4.method shared
 nmcli connection modify "$CONNECTION2" connection.interface-name "$INTERFACE2"
+nmcli connection modify "$CONNECTION2" ipv4.addresses 10.153.153.0/24
 nmcli connection modify "$CONNECTION2" ipv4.dhcp-client-id ""
 nmcli connection modify "$CONNECTION2" ipv4.dhcp-timeout 0
 nmcli connection modify "$CONNECTION2" ipv4.dhcp-send-hostname no
--- files/vm-resources/mint/04-mullvad-install.sh
+++ files/vm-resources/mint/04-mullvad-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
+if ! [[ $EUID -ne 0 ]]; then
 	echo "This script must not be run with root/sudo privileges."
 	exit 1
 fi
