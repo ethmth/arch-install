909cfb7cecd34a6910befcef934f32bcd36b79b5
ethmth <ethan@smaze.net>
2023-07-08 19:09:05 -0400

    Setup autossh service in script

--- files/vm-resources/mint/extension/05-autossh-setup.sh
+++ files/vm-resources/mint/extension/05-autossh-setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if ! [[ $EUID -ne 0 ]]; then
-	echo "This script should not be run with root/sudo privileges."
+if [[ $EUID -ne 0 ]]; then
+	echo "This script should be run with root/sudo privileges."
 	exit 1
 fi
-CUR_USER=$(whoami)
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+if ! [ -f "$SCRIPT_DIR/autossh.service" ]; then
+    echo "$SCRIPT_DIR/autossh.service not found"
+    exit 1
+fi
 
 echo "Running ssh-keygen"
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | ssh-keygen
@@ -16,16 +23,13 @@ EOF
 read -p "Enter your username on the Wireguard machine: " username
 
 mkdir -p /tmp/sshtemp/.ssh
-cp /home/$CUR_USER/.ssh/id_rsa.pub /tmp/sshtemp/.ssh/authorized_keys
+cp /root/.ssh/id_rsa.pub /tmp/sshtemp/.ssh/authorized_keys
 
 scp -r /tmp/sshtemp/.ssh $username@10.153.153.18:/home/$username/.ssh
 
-echo "@reboot /usr/bin/autossh -D 0.0.0.0:6969 -N $username@10.153.153.18" > /tmp/crontemp
-
-crontab /tmp/crontemp
-
-echo "Set crontab:"
-crontab -l
+cp $SCRIPT_DIR/autossh.service /etc/systemd/system/autossh.service
+sed -i "s/USERNAME/$username/g" /etc/systemd/system/autossh.service
+systemctl enable autossh.service
 
 echo "If ssh setup was successful, you should end up in the remote shell"
 echo "Exit. Then, reboot"
