772f1f4b6370106b9a15f07741807ee50e7aef3b
ethmth <ethan@smaze.net>
2023-07-09 08:21:39 -0400

    Added VPN-external, began changing mint extension setup instructions

--- files/vm-resources/mint/extension/02-network.sh
+++ files/vm-resources/mint/extension/02-network.sh
@@ -19,10 +19,10 @@ CONNECTION2="${CONNECTION2%% }"
 
 
 nmcli connection modify "$CONNECTION1" ipv4.method manual
-nmcli connection modify "$CONNECTION1" ipv4.addresses 10.152.152.17/18
+nmcli connection modify "$CONNECTION1" ipv4.addresses 10.0.3.15/18
 nmcli connection modify "$CONNECTION1" connection.interface-name "$INTERFACE1"
-nmcli connection modify "$CONNECTION1" ipv4.gateway 10.152.152.10
-nmcli connection modify "$CONNECTION1" ipv4.dns 10.152.152.10
+nmcli connection modify "$CONNECTION1" ipv4.gateway 10.0.3.0
+# nmcli connection modify "$CONNECTION1" ipv4.dns 10.152.152.10
 nmcli connection down "$CONNECTION1"
 nmcli connection up "$CONNECTION1"
 
--- scripts/vm/07-network-vpn-intext.sh
+++ scripts/vm/07-network-vpn-intext.sh
@@ -8,11 +8,15 @@ fi
 CUR_USER=$(whoami)
 
 # Setup VPN-Internal network
-sudo -k virsh -c qemu:///system net-define /home/$CUR_USER/arch-install/files/templates/VPN-Internal.xml
+sudo virsh -c qemu:///system net-define /home/$CUR_USER/arch-install/files/templates/VPN-Internal.xml
 sudo virsh -c qemu:///system net-autostart VPN-Internal
 sudo virsh -c qemu:///system net-start VPN-Internal
 
-echo "VPN-Internal should have been successfully defined"
+sudo virsh -c qemu:///system net-define /home/$CUR_USER/arch-install/files/templates/VPN-External.xml
+sudo virsh -c qemu:///system net-autostart VPN-External
+sudo virsh -c qemu:///system net-start VPN-External
+
+echo "VPN-Internal and VPN-External should have been successfully defined"
 echo "If you haven't yet, you may want to setup vfio by running the ../10-vfio/ scripts"
 echo "Otherwise, you may want to setup Whonix by running the ../11-whonix/ scripts."
 echo "Alternatively, define a bridged network using ./08-network-bridged.sh (WORK IN PROGRESS)"
