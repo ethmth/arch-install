3b8ff051c6fd07f0aa15694c2a717a8eae26b2a4
ethmth <ethan@smaze.net>
2023-07-13 17:27:30 -0400

    Started archivebox setup

---
+++ util/misc/archivebox/archivebox-install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+NAME="archivebox"
+
+if ! [[ $EUID -ne 0 ]]; then
+        echo "This script should not be run with root/sudo privileges."
+        exit 1
+fi
+
+CUR_USER=$(whoami)
+
+if ! [ -e "../$NAME/docker-compose.yml" ]; then
+	echo "Make sure you run this script in the same directory as ../$NAME/docker-compose.yml"
+	exit 1
+fi
+
+
+LOC=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | fzf --prompt="Select your desired Label Studio installation location")
+
+if ([ "$LOC" == "" ] || [ "$LOC" == "Cancel" ]); then
+    echo "Nothing was selected"
+    echo "Run this script again with target drive mounted."
+    exit 1
+fi
+
+if [ "$LOC" == "/" ]; then
+    LOC="/home/$CUR_USER"
+fi
+
+if ! [ -d "$LOC" ]; then
+    echo "Your location is not available. Is the disk mounted? Do you have access?"
+	exit 1
+fi
+
+LOC="$LOC/programs"
+mkdir -p $LOC
+
+if ! [ -e "$LOC/$NAME/data/sonic" ]; then
+mkdir -p $LOC/$NAME/data/sonic
+chmod -R 777 $LOC/$NAME/data
+fi
+
+if ! [ -e "$LOC/$NAME/etc" ]; then
+mkdir -p $LOC/$NAME/etc
+chmod -R 777 $LOC/$NAME/etc
+fi
+
+cp docker-compose.yml $LOC/$NAME/docker-compose.yml
+cp sonic.cfg $LOC/$NAME/sonic.cfg
+
+cd $LOC/$NAME/
+docker compose up --build -d
+
+docker compose run archivebox manage createsuperuser
+
+# sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | docker compose run -T archivebox manage createsuperuser
+#     # use default user
+#     # empty email
+#   password # make password password
+#   password # confirm password
+#   y # confirm unsafe pass
+# EOF
+
+echo "$NAME installed in $LOC and started."
+echo "Run 'docker compose up --build -d' to run it and 'docker compose stop' to stop it."
+echo "cd $LOC/$NAME"
