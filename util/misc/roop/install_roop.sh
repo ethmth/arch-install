7c286decee13c8a46673fb542d84c4f7bde10c98
ethmth <ethan@smaze.net>
2023-07-20 15:27:57 -0400

    Started roop installation script

---
+++ util/misc/roop/install_roop.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if ! [[ $EUID -ne 0 ]]; then
+        echo "This script should not be run with root/sudo privileges."
+        exit 1
+fi
+
+CUR_USER=$(whoami)
+
+LOC=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | fzf --prompt="Select your desired SD installation location")
+
+if ([ "$LOC" == "" ] || [ "$LOC" == "Cancel" ]); then
+    echo "Nothing was selected"
+    echo "Run this script again with target drive mounted."
+    exit 1
+fi
+
+if [ "$LOC" == "/" ]; then
+    LOC="/home/$CUR_USER"
+fi
+
+if ! [ -d "$LOC" ]; then
+    echo "Your location is not available. Is the disk mounted? Do you have access?"
+	exit 1
+fi
+
+LOC="$LOC/programs"
+mkdir -p $LOC
+
+git clone --depth 1 https://github.com/s0md3v/roop.git $LOC/roop
+
+cd $LOC/roop
+
+python -m venv .venv
+
+source $LOC/roop/.venv/bin/activate
+
+# $LOC/roop/.venv/bin/python -m $LOC/roop/.venv/bin/pip install -r requirements.txt
+# $LOC/roop/.venv/bin/python -m $LOC/roop/.venv/bin/pip install onnxruntime-gpu==1.15.1
+
+$LOC/roop/.venv/bin/pip install -r requirements.txt
+$LOC/roop/.venv/bin/pip install onnxruntime-gpu==1.15.1
+
+echo "#!/bin/bash" > $LOC/roop/run.sh
+echo "source $LOC/roop/.venv/bin/activate" >> $LOC/roop/run.sh
+echo "$LOC/roop/.venv/bin/python run.py --execution-provider cuda" >> $LOC/roop/run.sh
+
+chmod +rx $LOC/roop/run.sh
+
+echo "Installed roop in $LOC"
+echo "Run ./run.sh to start it"
+echo "cd $LOC/roop"
