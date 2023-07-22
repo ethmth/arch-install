e586a294384dd8727bf25a062107a21332cb194f
ethmth <ethan@smaze.net>
2023-07-22 13:09:02 -0400

    Started tts installation

---
+++ util/misc/ai-creation/voice-cloning-app/install-voice-cloning-app.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+NAME="Voice-Cloning-App"
+
+if ! [[ $EUID -ne 0 ]]; then
+        echo "This script should not be run with root/sudo privileges."
+        exit 1
+fi
+
+CUR_USER=$(whoami)
+
+if ! [ -e "docker-compose.yml" ]; then
+    echo "Docker compose file not found"
+    exit 1
+fi
+
+LOC=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | fzf --prompt="Select your desired $NAME installation location")
+
+if ([ "$LOC" == "" ] || [ "$LOC" == "Cancel" ]); then
+    echo "Nothing was selected"
+    echo "Run this script again with target drive mounted."
+    exit 1
+fi
+
+if [ "$LOC" == "/" ]; then
+    LOC="/home/$CUR_USER"
+fi
+
+if ! [ -d "$LOC" ]; then
+    echo "Your location is not available. Is the disk mounted? Do you have access?"
+	exit 1
+fi
+
+DISK=$LOC
+LOC="$LOC/programs"
+mkdir -p $LOC
+
+git clone https://github.com/BenAAndrew/Voice-Cloning-App.git $LOC/$NAME
+cp docker-compose.yml $LOC/$NAME/docker-compose.yml
+
+echo "Docker version installed to $LOC."
+echo "Run 'docker compose up --build' to start."
+echo "cd $LOC/$NAME"
