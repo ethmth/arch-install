34ebf8ea205920274596e8560516c86f7914913a
ethmth <ethan@smaze.net>
2023-07-22 13:38:01 -0400

    Switched to custom dockerfile for tts

--- util/misc/ai-creation/tts/install-tts.sh
+++ util/misc/ai-creation/tts/install-tts.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-NAME="tts"
+CONTAINER_NAME="tts"
+
+VOLUMES="
+tts-output
+"
+
+FILES="
+docker-compose.yml
+Dockerfile
+"
 
 if ! [[ $EUID -ne 0 ]]; then
         echo "This script should not be run with root/sudo privileges."
@@ -9,35 +18,41 @@ fi
 
 CUR_USER=$(whoami)
 
-if ! [ -e "docker-compose.yml" ]; then
-    echo "Docker compose file not found"
-    exit 1
-fi
+mkdir -p /home/$CUR_USER/programs/$CONTAINER_NAME
 
-LOC=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | fzf --prompt="Select your desired $NAME installation location")
+for file in $FILES; do
+    if ! [ -e "../$CONTAINER_NAME/$file" ]; then
+            echo "$file doesn't exist. Make sure you run this script in the same directory as ../$CONTAINER_NAME/$file"
+            exit 1
+    fi
+done
 
+LOC=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | fzf --prompt="Select your desired $CONTAINER_NAME installation location")
 if ([ "$LOC" == "" ] || [ "$LOC" == "Cancel" ]); then
     echo "Nothing was selected"
     echo "Run this script again with target drive mounted."
     exit 1
 fi
-
 if [ "$LOC" == "/" ]; then
     LOC="/home/$CUR_USER"
 fi
-
 if ! [ -d "$LOC" ]; then
     echo "Your location is not available. Is the disk mounted? Do you have access?"
 	exit 1
 fi
-
-DISK=$LOC
 LOC="$LOC/programs"
-mkdir -p $LOC
 
-git clone https://github.com/coqui-ai/TTS.git $LOC/$NAME
-cp docker-compose.yml $LOC/$NAME/docker-compose.yml
+mkdir -p $LOC/$CONTAINER_NAME
 
-echo "Docker version installed to $LOC."
-echo "Run 'docker compose up --build' to start."
-echo "cd $LOC/$NAME"
+for file in $FILES; do
+    cp $file $LOC/$CONTAINER_NAME/$file
+done
+
+for vol in $VOLUMES; do
+    mkdir $LOC/$CONTAINER_NAME/$vol
+    chmod -R 777 $LOC/$CONTAINER_NAME/$vol
+done
+
+echo "Installed $CONTAINER_NAME to $LOC"
+echo "Run 'docker compose up --build' to start"
+echo "cd $LOC/$CONTAINER_NAME"
