04b71a45605b111fe096201091a7b4b8335b6e52
ethmth <ethan@smaze.net>
2023-07-21 20:38:47 -0400

    Backup scheme works

--- util/misc/sd/backup-model.sh
+++ util/misc/sd/backup-model.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-if ! [[ $EUID -ne 0 ]]; then
-        echo "This script should not be run with root/sudo privileges."
-        exit 1
-fi
-
-
 IGNORE_LIST="
 v1-5-pruned-emaonly.safetensors
 "
 
+if ! [[ $EUID -ne 0 ]]; then
+        echo "This script should not be run with root/sudo privileges."
+        exit 1
+fi
 CUR_USER=$(whoami)
+
+BACKUP_PATH="/home/$CUR_USER/.local/share/Cryptomator/mnt/SecureModels/stable-diffusion-webui"
+
+if ! [ -e "$BACKUP_PATH/index.txt" ]; then
+    echo "Cryptomator/mnt/SecureModels/stable-diffusion-webui/index.txt is not available"
+    exit 1
+fi
+
+mkdir -p $BACKUP_PATH/backups
+
 
 MNT_PTS=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | grep -v -x "/")
 MNT_ARRAY=()
@@ -44,21 +52,23 @@ if ! (( ALL_OF_THEM )); then
         new_files=$(printf "$new_file\n$new_files")
     done <<< "$desired_files"
     files=$new_files
+else
+    desired_files=$short_list
 fi
 
-echo "$files" 
+while IFS= read -r file; do
+    short_file=$(echo "$file" | awk -F "stable-diffusion-webui/" '{print $2}')
 
+    EXISTS=$(cat "$BACKUP_PATH/index.txt" | grep "$short_file" | wc -l)
 
-# filtered_files=()
+    random_name=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)
 
-# # Loop through each element in the file_list (null-delimited)
-# while IFS= read -r -d '' item; do
-#     # Check if the current item is a regular file
-#     if [ -f "$item" ]; then
-#         # If it's a file, add it to the filtered_files array
-#         filtered_files+=("$item")
-#     fi
-# done <<< "$file_list"
+    if (( EXISTS )); then
+        echo "$short_file already exists"
+    else
+        echo "$short_file,$random_name" >> "$BACKUP_PATH/index.txt"
+        cp $file $BACKUP_PATH/backups/$random_name
+    fi
+done <<< "$files"
 
-# # Print the filtered files (null-delimited)
-# printf '%s\0' "${filtered_files[@]}"
+echo "Models backed up. Check $BACKUP_PATH/index.txt and $BACKUP_PATH/backups for sanity"
