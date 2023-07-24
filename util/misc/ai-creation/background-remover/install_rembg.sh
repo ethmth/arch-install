44f93dee38e763c0bc5d894ed99213d30c57579a
ethmth <ethan@smaze.net>
2023-07-24 15:57:27 -0400

    Started install_rembg

---
+++ util/misc/ai-creation/background-remover/install_rembg.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+NAME="rembg"
+
+if ! [[ $EUID -ne 0 ]]; then
+        echo "This script should not be run with root/sudo privileges."
+        exit 1
+fi
+
+CUR_USER=$(whoami)
+
+LOC=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | fzf --prompt="Select your desired $NAME installation location")
+
+if ([ "$LOC" == "" ] || [ "$LOC" == "Cancel" ]); then
+    echo "Nothing was selected"
+    echo "Run this script again with target drive mounted."
+    exit 1
+fi
+
+if [ "$LOC" == "/" ]; then
+    LOC="/home/$CUR_USER"
+fi
+
+if ! [ -d "$LOC" ]; then
+    echo "Your location is not available. Is the disk mounted? Do you have access?"
+	exit 1
+fi
+
+LOC="$LOC/programs"
+mkdir -p $LOC
+
+git clone --depth 1 https://github.com/danielgatis/rembg.git $LOC/$NAME
+
+cd $LOC/$NAME
+
+python -m venv $LOC/$NAME/.venv
+
+source $LOC/$NAME/.venv/bin/activate
+
+$LOC/$NAME/.venv/bin/python $LOC/$NAME/setup.py
