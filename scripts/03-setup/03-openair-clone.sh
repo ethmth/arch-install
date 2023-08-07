b795c66d5bc6877b70bc47d5cc579c40d3e389ad
ethmth <ethan@smaze.net>
2023-08-07 14:32:40 -0400

    Updated notes for openair clone

--- scripts/03-setup/03-openair-clone.sh
+++ scripts/03-setup/03-openair-clone.sh
@@ -17,9 +17,13 @@ cp /home/$CUR_USER/openair-vpn/vars/install_location.conf.example /home/$CUR_USE
 sed -i "s/eth0/$NETWORK_INTERFACE/g" /home/$CUR_USER/openair-vpn/vars/vars.conf
 sed -i "s|/home/me|/home/$CUR_USER|g" /home/$CUR_USER/openair-vpn/vars/vars.conf
 
+
 vim /home/$CUR_USER/openair-vpn/vars/vars.conf
 
+mkdir -p /home/$CUR_USER/.vpn
+
 echo "Navigate to https://airvpn.org/generator/ and download AirVPN.zip into ~/.vpn/"
+echo "Select Linux, OpenVPN TCP 443, Single Server (Invert Selection to Select All) "
 echo "Edit /home/$CUR_USER/openair-vpn/vars/vars.conf"
 echo "When DONE EDITING vars.conf, run ./04-openair-install.sh"
 echo "vim /home/$CUR_USER/openair-vpn/vars/vars.conf"
