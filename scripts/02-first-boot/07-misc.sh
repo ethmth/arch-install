5c583a14e28050a3bcf50d678baf9ffe8f728a64
ethmth <ethan@smaze.net>
2023-08-12 12:51:04 -0400

    Added escape sequence in bashrc for foot terminal

--- scripts/02-first-boot/07-misc.sh
+++ scripts/02-first-boot/07-misc.sh
@@ -69,6 +69,22 @@ echo "export ANDROID_HOME=\"/home/$CUR_USER/Android/Sdk\"" >> /home/$CUR_USER/.b
 # npm user setup
 echo "export npm_config_prefix=\"\$HOME/.local\"" >> /home/$CUR_USER/.bashrc
 
+echo "osc7_cwd() {
+    local strlen=\${#PWD}
+    local encoded=\"\"
+    local pos c o
+    for (( pos=0; pos<strlen; pos++ )); do
+        c=\${PWD:\$pos:1}
+        case \"\$c\" in
+            [-/:_.!\'\(\)~[:alnum:]] ) o=\"\${c}\" ;;
+            * ) printf -v o '%%%02X' \"'\${c}\" ;;
+        esac
+        encoded+=\"\${o}\"
+    done
+    printf '\e]7;file://%s%s\e\\\' \"\${HOSTNAME}\" \"\${encoded}\"
+}
+PROMPT_COMMAND=\${PROMPT_COMMAND:+\$PROMPT_COMMAND; }osc7_cwd" >> /home/$CUR_USER/.bashrc
+
 # Docker container runtime setup
 if (( NVIDIA )); then
 string_to_echo=$(echo '{
