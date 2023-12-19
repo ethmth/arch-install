a2db01cde463829145cfa606049601c662ffff3a
ethmth <ethan@smaze.net>
2023-12-19 15:48:22 -0500

    Added Pinta

--- scripts/03-setup/02-flatpaks.sh
+++ scripts/03-setup/02-flatpaks.sh
@@ -10,6 +10,7 @@ source /home/$CUR_USER/arch-install/config/system.conf
 
 # Install Flatpaks
 flatpaks="
+com.github.PintaProject.Pinta
 org.upscayl.Upscayl
 io.dbeaver.DBeaverCommunity
 com.visualstudio.code
