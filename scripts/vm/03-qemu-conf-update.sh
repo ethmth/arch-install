9eab06fd0708781441777c1ac86ab7cda49ad047
ethmth <ethan@smaze.net>
2023-12-28 10:57:46 -0500

    Switched qemu config to root user

--- scripts/vm/03-qemu-conf-update.sh
+++ scripts/vm/03-qemu-conf-update.sh
@@ -7,8 +7,8 @@ fi
 
 CUR_USER=$(whoami)
 
-sudo -k bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/libvirt/qemu.conf "user=\"$CUR_USER\""
-sudo bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/libvirt/qemu.conf "group=\"libvirt\""
+sudo -k bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/libvirt/qemu.conf "user=\"root\""
+sudo bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/libvirt/qemu.conf "group=\"root\""
 
 echo "/etc/libvirt/qemu.conf updated with correct user information."
 echo "Run ./04-resources-disk.sh to create the resources disk."
