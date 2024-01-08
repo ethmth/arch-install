31aaa0b948f26899c68f0b98d167d000daf5a797
ethmth <ethan@smaze.net>
2024-01-08 03:59:45 -0500

    Added git config version change

--- files/vm-resources/ubuntu/06-user-configuration.sh
+++ files/vm-resources/ubuntu/06-user-configuration.sh
@@ -41,3 +41,4 @@ echo "Host phone
 echo "export SSHPASS=\"password\"" >> /home/$CUR_USER/.bashrc
 
 git config --global alias.pr '!f() { git fetch -fu ${2:-origin} refs/pull/$1/head:pr/$1 && git checkout pr/$1; }; f'
+git config --global http.version HTTP/1.1
