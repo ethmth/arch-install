5b8821b1e71300dcb0cf877a6d34f992878cec3e
ethmth <ethan@smaze.net>
2024-01-16 19:12:39 -0500

    Added vmware-workstation

---
+++ scripts/03-setup/08-vmware-services.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! [[ $EUID -ne 0 ]]; then
+	echo "This script should not be run with root/sudo privileges."
+	exit 1
+fi
+
+sudo systemctl start vmware-networks-configuration.service
+sudo systemctl enable vmware-networks.service
+sudo systemctl enable vmware-usbarbitrator.service
