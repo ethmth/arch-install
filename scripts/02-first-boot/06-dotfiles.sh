1efc1db5c065f6a164456ccd576563c1e4a26497
ethmth <ethan@smaze.net>
2024-03-12 14:09:00 -0400

    Updated dotfiles install script

--- scripts/02-first-boot/06-dotfiles.sh
+++ scripts/02-first-boot/06-dotfiles.sh
@@ -14,14 +14,22 @@ git clone https://github.com/ethmth/arch-dots.git /home/$CUR_USER/arch-dots
 shopt -s dotglob
 
 cp -r /home/$CUR_USER/arch-dots/home/* /home/$CUR_USER/
-# TODO - change DEVICE variable in this hyprmon script depending on device:
+
+if (( LAPTOP )); then
+bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /home/$CUR_USER/.config/hypr/hyprmon.sh 'DEVICE="laptop"'
+elif (( DESKTOP )); then
+bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /home/$CUR_USER/.config/hypr/hyprmon.sh 'DEVICE="desktop"'
+fi
+
 sudo cp /home/$CUR_USER/.config/hypr/hyprmon.sh /usr/bin/hyprmon
 sudo chmod +rx /usr/bin/hyprmon
 
 sed -i "s/eth0/$NETWORK_INTERFACE/g" /home/$CUR_USER/.config/waybar/config.jsonc
 rm -rf /home/$CUR_USER/arch-dots
 
-echo "source ~/.config/bash/prompt" >> /home/$CUR_USER/.bashrc
+if ! ( cat "/home/$CUR_USER/.bashrc" | grep "source ~/.config/bash/prompt" ); then
+	echo "source ~/.config/bash/prompt" >> /home/$CUR_USER/.bashrc
+fi
 
 echo "Verify that dotfiles were installed correctly"
 echo "If so, run ./07-misc.sh"
