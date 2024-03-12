05b656707f26076ea7b13e5fb87e6d52b1eebb57
ethmth <ethan@smaze.net>
2024-03-12 14:49:36 -0400

    Conda init check fixed

--- scripts/03-setup/06-conda-init.sh
+++ scripts/03-setup/06-conda-init.sh
@@ -6,6 +6,9 @@ if ! [[ $EUID -ne 0 ]]; then
 fi
 
 CUR_USER=$(whoami)
+
+if ! ( cat "/home/$CUR_USER/.bashrc" | grep -q '>>> conda initialize >>>' ); then
 source /opt/anaconda/bin/activate
 conda init bash
 conda config --set auto_activate_base false
+fi
