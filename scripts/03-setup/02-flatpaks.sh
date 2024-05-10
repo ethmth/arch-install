3b75791944f95430d2fa014dc8477652d51e9593
ethmth <ethan@smaze.net>
2024-05-09 23:56:12 -0400

    Added pdfarranger

--- scripts/03-setup/02-flatpaks.sh
+++ scripts/03-setup/02-flatpaks.sh
@@ -11,6 +11,7 @@ source /home/$CUR_USER/arch-install/config/system.conf
 # Install Flatpaks
 # nz.mega.MEGAsync
 flatpaks="
+com.github.jeromerobert.pdfarranger
 io.github.dman95.SASM
 org.kde.kleopatra
 com.github.libresprite.LibreSprite
