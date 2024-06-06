5d8b2497e61efa725d9c396795421d638d0b39b1
ethmth <ethan@smaze.net>
2024-06-06 14:59:38 -0400

    Updated preview config

--- files/vm-resources/ubuntu/nextcloud/00-docker/preview-config.sh
+++ files/vm-resources/ubuntu/nextcloud/00-docker/preview-config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# enable previews
 docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enable_previews --value=true --type=boolean" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set preview_max_memory --value=-1 --type=integer" www-data
+
 
 docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 0 --value='OC\\Preview\\Movie'" www-data
 docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 1 --value='OC\\Preview\\PNG'" www-data
@@ -14,6 +17,19 @@ docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:syste
 docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 9 --value='OC\\Preview\\MarkDown'" www-data
 docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 10 --value='OC\\Preview\\PDF'" www-data
 
+# add trusted domain
 docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set trusted_domains 2 --value='10.152.153.14'" www-data
 
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set localstorage.umask --value='0000'" www-data
+# set umask
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set localstorage.umask --value=0000" www-data
+
+# enable External Files app
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ app:enable files_external" www-data
+
+# disable update checker/internet checks
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set updatechecker --value=false --type=boolean" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set has_internet_connection --value=false --type=boolean" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set upgrade.disable-web --value=true --type=boolean" www-data
+
+# disable filelocking
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set filelocking.enabled --value=false --type=boolean" www-data
