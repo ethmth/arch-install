69682f7d218dd44ba1f403ca7e2c9d5cb0e21b6f
ethmth <ethan@smaze.net>
2024-06-06 17:22:50 -0400

    Fixed fstab mount

--- files/vm-resources/ubuntu/nextcloud/02-fstab-webdav.sh
+++ files/vm-resources/ubuntu/nextcloud/02-fstab-webdav.sh
@@ -7,6 +7,8 @@ fi
 
 CUR_USER=$(whoami)
 
-fstab_string="http://localhost:8080/remote.php/dav/files/admin/Local /mnt/nextcloud davfs rw,user,uid=$CUR_USER,noauto 0 0"
+sudo mkdir -p /mnt/nextcloud
+
+fstab_string="http://localhost:8080/remote.php/dav/files/admin/Local /mnt/nextcloud davfs rw,user,uid=$CUR_USER,_netdav 0 0"
 sudo sh -c "echo \"$fstab_string\" >> /etc/fstab"
 
