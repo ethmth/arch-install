bf5f6bc05ec09fbf7a74197c280d4e7d406a87ef
ethmth <ethan@smaze.net>
2024-06-06 15:18:36 -0400

    Changed media to mnt

--- files/vm-resources/ubuntu/nextcloud/02-fstab-webdav.sh
+++ files/vm-resources/ubuntu/nextcloud/02-fstab-webdav.sh
@@ -7,6 +7,6 @@ fi
 
 CUR_USER=$(whoami)
 
-fstab_string="http://localhost:8080/remote.php/dav/files/admin/Local /media/nextcloud davfs rw,user,uid=$CUR_USER,noauto 0 0"
+fstab_string="http://localhost:8080/remote.php/dav/files/admin/Local /mnt/nextcloud davfs rw,user,uid=$CUR_USER,noauto 0 0"
 sudo sh -c "echo \"$fstab_string\" >> /etc/fstab"
 
