3a5162005439c11f2d3b46527b92789e94d99cfd
ethmth <ethan@smaze.net>
2024-06-07 18:56:30 -0400

    Added virsh-net-start script

--- scripts/02-first-boot/08-scripts.sh
+++ scripts/02-first-boot/08-scripts.sh
@@ -51,7 +51,7 @@ sudo cp /home/$CUR_USER/arch-install/files/installed_scripts/pdf2png /usr/bin/pd
 sudo cp /home/$CUR_USER/arch-install/files/installed_scripts/nightlight /usr/bin/nightlight
 sudo cp /home/$CUR_USER/arch-install/files/installed_scripts/svg2ico /usr/bin/svg2ico
 sudo cp /home/$CUR_USER/arch-install/files/installed_scripts/get-cookies /usr/bin/get-cookies
-
+sudo cp /home/$CUR_USER/arch-install/files/installed_scripts/virsh-net-start /usr/bin/virsh-net-start
 
 sudo chmod +rx /etc/openvpn/update-resolv-conf
 sudo chmod +rx /usr/bin/sshbg
@@ -67,6 +67,7 @@ sudo chmod +rx /usr/bin/pdf2png
 sudo chmod +rx /usr/bin/nightlight
 sudo chmod +rx /usr/bin/svg2ico
 sudo chmod +rx /usr/bin/get-cookies
+sudo chmod +rx /usr/bin/virsh-net-start
 
 echo "Verify that scripts were installed correctly (sshbg, stream-dl, megasync-delay, etc)"
 echo "If so, run ./09-initcpio.sh"
