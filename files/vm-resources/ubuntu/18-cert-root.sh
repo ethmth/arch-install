0804bbbe403c54d82ad8c8009bf3fb1c09b6528d
ethmth <ethan@smaze.net>
2024-06-08 15:43:31 -0400

    updated cert scripts

--- files/vm-resources/ubuntu/18-cert-root.sh
+++ files/vm-resources/ubuntu/18-cert-root.sh
@@ -5,7 +5,7 @@ if ! [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-if [ -d "~/certs" ]; then
+if [ -d "$HOME/certs" ]; then
     echo "~/certs already exists. Remove it to continue"
     exit 1
 fi
@@ -13,20 +13,35 @@ fi
 mkdir -p ~/certs
 cd ~/certs
 
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | openssl genrsa -des3 -out myCA.key 2048
-    # no password
-    # no password confirm
-EOF
+#sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | openssl genrsa -des3 -out myCA.key 2048
+## no password
+## no password confirm
+#EOF
 
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem
-    # no password
-    .
-    .
-    .
-    .
-    .
-    My Epic Cert
-    .
+export PASSWORD=""
+openssl genrsa -des3 -passout env:PASSWORD -out myCA.key 2048
+
+#exit 0
+
+#sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem
+#    # no password
+#    .
+#    .
+#    .
+#    .
+#    .
+#    My Epic Cert
+#    .
+#EOF
+export PASSWORD=""
+openssl req -x509 -new -nodes -key myCA.key -sha256 -passin env:PASSWORD -days 36500 -out myCA.pem <<EOF
+.
+.
+.
+.
+.
+Nextcloud Local Common Name
+.
 EOF
 
 sudo cp ~/certs/myCA.pem /usr/local/share/ca-certificates/myCA.crt
--- files/vm-resources/ubuntu/19-cert-signed.sh
+++ files/vm-resources/ubuntu/19-cert-signed.sh
@@ -5,26 +5,52 @@ if ! [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-if ! [ -d "~/certs" ]; then
+if ! [ -d "$HOME/certs" ]; then
     echo "~/certs doesn't exist."
     exit 1
 fi
 
+if ! [ -f "nextcloudlocal.test.ext" ]; then
+    echo "No nextcloudlocal.test.ext"
+    exit 1
+fi
+
+cp nextcloudlocal.test.ext ~/certs
+
 cd ~/certs
 
-openssl genrsa -out nextcloud.test.key 2048
+openssl genrsa -out nextcloudlocal.test.key 2048
 
-
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem
-    # no password
-    .
-    .
-    .
-    .
-    .
-    nextcloud
-    .
-    .
-    .
+openssl req -new -key nextcloudlocal.test.key -out nextcloudlocal.test.csr <<EOF
+.
+.
+.
+.
+.
+Nextcloud Local Common Name
+.
+.
+.
 EOF
-openssl req -new -key nextcloud.test.key -out nextcloud.test.csr
+
+#exit 0
+
+#sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem
+#    # no password
+#    .
+#    .
+#    .
+#    .
+#    .
+#    nextcloud
+#    .
+#    .
+#    .
+#EOF
+#
+#openssl req -new -key nextcloud.test.key -out nextcloud.test.csr
+
+export PASSWORD=""
+openssl x509 -req -in nextcloudlocal.test.csr -CA myCA.pem -CAkey myCA.key -passin env:PASSWORD \
+-CAcreateserial -out nextcloudlocal.test.crt -days 825 -sha256 -extfile nextcloudlocal.test.ext
+
