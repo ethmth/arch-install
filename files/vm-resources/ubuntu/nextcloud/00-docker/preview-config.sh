6bf19c4f44f17e82d899b604abbb93130a164de8
ethmth <ethan@smaze.net>
2024-06-08 03:02:08 -0400

    Added imaginary

--- files/vm-resources/ubuntu/nextcloud/00-docker/preview-config.sh
+++ files/vm-resources/ubuntu/nextcloud/00-docker/preview-config.sh
@@ -6,18 +6,30 @@ docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:syste
 
 
 docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 0 --value='OC\\Preview\\Movie'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 1 --value='OC\\Preview\\PNG'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 2 --value='OC\\Preview\\JPEG'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 3 --value='OC\\Preview\\GIF'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 4 --value='OC\\Preview\\BMP'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 5 --value='OC\\Preview\\XBitmap'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 6 --value='OC\\Preview\\MP3'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 7 --value='OC\\Preview\\MP4'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 8 --value='OC\\Preview\\TXT'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 9 --value='OC\\Preview\\MarkDown'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 10 --value='OC\\Preview\\PDF'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 11 --value='OC\\Preview\\RIFF'" www-data
-docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 12 --value='OC\\Preview\\WEBP'" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 1 --value='OC\\Preview\\Imaginary'" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 2 --value='OC\\Preview\\MP3'" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 3 --value='OC\\Preview\\MP4'" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 4 --value='OC\\Preview\\TXT'" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 5 --value='OC\\Preview\\MarkDown'" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 6 --value='OC\\Preview\\PDF'" www-data
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 7 --value='OC\\Preview\\GIF'" www-data
+
+docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set preview_imaginary_url --value='http://host.docker.internal:9000'" www-data
+
+
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 0 --value='OC\\Preview\\Movie'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 1 --value='OC\\Preview\\PNG'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 2 --value='OC\\Preview\\JPEG'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 3 --value='OC\\Preview\\GIF'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 4 --value='OC\\Preview\\BMP'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 5 --value='OC\\Preview\\XBitmap'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 6 --value='OC\\Preview\\MP3'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 7 --value='OC\\Preview\\MP4'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 8 --value='OC\\Preview\\TXT'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 9 --value='OC\\Preview\\MarkDown'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 10 --value='OC\\Preview\\PDF'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 11 --value='OC\\Preview\\RIFF'" www-data
+# docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set enabledPreviewProviders 12 --value='OC\\Preview\\WEBP'" www-data
 
 # add trusted domain
 docker exec -it nextcloud-app su -s /bin/bash -c "/var/www/html/occ config:system:set trusted_domains 2 --value='10.152.153.14'" www-data
