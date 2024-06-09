e0b9039396431d994e128b52ee8324407a601f48
ethmth <ethan@smaze.net>
2024-06-08 23:44:01 -0400

    Edited service scripts

--- files/vm-resources/ubuntu/nextcloud/03-systemd-nextcloud.sh
+++ files/vm-resources/ubuntu/nextcloud/03-systemd-nextcloud.sh
@@ -16,12 +16,13 @@ sudo cp check_and_delete_mount.sh /usr/local/bin/check_and_delete_mount.sh
 sudo chmod +rx /usr/local/bin/check_and_delete_mount.sh
 
 CHECK_CONTENTS="[Unit]
-Description=Check if /mnt/veracrypt1 is a mountpoint on startup. If not, delete it.
-After=local-fs.target remote-fs.target
-Before=docker.service
+Description=Check for mountpoint on startup. Delete folder if it exists.
+After=local-fs.target remote-fs.target nextcloud-down.service
+Requires=nextcloud-down.service
 
 [Service]
 Type=oneshot
+ExecStartPre=/bin/sleep 1
 ExecStart=/usr/local/bin/check_and_delete_mount.sh
 
 [Install]
@@ -30,12 +31,12 @@ WantedBy=multi-user.target
 
 DOWN_CONTENTS="[Unit]
 Description=Run docker-compose down on startup.
-After=network.target check-mount.service docker.service
-Requires=check-mount.service
+After=local-fs.target remote-fs.target network.target docker.service
 
 [Service]
 Type=oneshot
 WorkingDirectory=/home/$CUR_USER/programs/nextcloud
+ExecStartPre=/bin/sleep 1
 ExecStart=/usr/bin/docker compose down
 User=$CUR_USER
 
@@ -45,12 +46,13 @@ WantedBy=multi-user.target
 
 UP_CONTENTS="[Unit]
 Description=Run docker-compose up on nextcloud on startup.
-After=nextcloud-down.service check-mount.service docker.service
-Requires=nextcloud-down.service check-mount.service
+After=check-mount.service docker.service
+Requires=check-mount.service
 
 [Service]
 Type=oneshot
 WorkingDirectory=/home/$CUR_USER/programs/nextcloud
+ExecStartPre=/bin/sleep 1
 ExecStart=/usr/bin/docker compose up -d
 User=$CUR_USER
 
@@ -58,9 +60,9 @@ User=$CUR_USER
 WantedBy=multi-user.target
 "
 
-sudo sh -c "echo \"$CHECK_CONTENTS\" >> /etc/systemd/system/check-mount.service"
-sudo sh -c "echo \"$DOWN_CONTENTS\" >> /etc/systemd/system/nextcloud-down.service"
-sudo sh -c "echo \"$UP_CONTENTS\" >> /etc/systemd/system/nextcloud-up.service"
+sudo sh -c "echo \"$CHECK_CONTENTS\" > /etc/systemd/system/check-mount.service"
+sudo sh -c "echo \"$DOWN_CONTENTS\" > /etc/systemd/system/nextcloud-down.service"
+sudo sh -c "echo \"$UP_CONTENTS\" > /etc/systemd/system/nextcloud-up.service"
 sudo systemctl enable check-mount.service
 sudo systemctl enable nextcloud-down.service
 sudo systemctl enable nextcloud-up.service
--- files/vm-resources/ubuntu/nextcloud/check_and_delete_mount.sh
+++ files/vm-resources/ubuntu/nextcloud/check_and_delete_mount.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# sleep 15
-
 MOUNT_POINT="/media/veracrypt1"
 
 if [ -d "$MOUNT_POINT" ]; then
-    # if ! [ -d "$MOUNT_POINT/ext" ]; then
-    #     echo "$MOUNT_POINT exists. Deleting..."
-    #     rm -rf "$MOUNT_POINT"
-    # fi
     rmdir "$MOUNT_POINT"
 fi
-
-
-# if ! mountpoint -q "$MOUNT_POINT"; then
-#     echo "$MOUNT_POINT is not a mountpoint. Deleting..."
-#     rm -rf "$MOUNT_POINT"
-# else
-#     echo "$MOUNT_POINT is a mountpoint. No action taken."
-# fi
