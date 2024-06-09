3f15c33e73df180cb5ab5cb53de414b96ea1cc43
ethmth <ethan@smaze.net>
2024-06-08 23:32:03 -0400

    Trying folder removal systemd service

--- files/vm-resources/ubuntu/nextcloud/03-systemd-nextcloud.sh
+++ files/vm-resources/ubuntu/nextcloud/03-systemd-nextcloud.sh
@@ -7,10 +7,31 @@ fi
 
 CUR_USER=$(whoami)
 
+if ! [ -f "check_and_delete_mount.sh" ]; then
+	echo "check_and_delete_mount.sh doesn't exist."
+	exit 1
+fi
+
+sudo cp check_and_delete_mount.sh /usr/local/bin/check_and_delete_mount.sh
+sudo chmod +rx /usr/local/bin/check_and_delete_mount.sh
+
+CHECK_CONTENTS="[Unit]
+Description=Check if /mnt/veracrypt1 is a mountpoint on startup. If not, delete it.
+After=local-fs.target remote-fs.target
+Before=docker.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/check_and_delete_mount.sh
+
+[Install]
+WantedBy=multi-user.target
+"
 
 DOWN_CONTENTS="[Unit]
 Description=Run docker-compose down on startup.
-After=network.target
+After=network.target check-mount.service docker.service
+Requires=check-mount.service
 
 [Service]
 Type=oneshot
@@ -22,12 +43,10 @@ User=$CUR_USER
 WantedBy=multi-user.target
 "
 
-
-
 UP_CONTENTS="[Unit]
 Description=Run docker-compose up on nextcloud on startup.
-After=nextcloud-down.service
-Requires=nextcloud-down.service
+After=nextcloud-down.service check-mount.service docker.service
+Requires=nextcloud-down.service check-mount.service
 
 [Service]
 Type=oneshot
@@ -39,8 +58,10 @@ User=$CUR_USER
 WantedBy=multi-user.target
 "
 
+sudo sh -c "echo \"$CHECK_CONTENTS\" >> /etc/systemd/system/check-mount.service"
 sudo sh -c "echo \"$DOWN_CONTENTS\" >> /etc/systemd/system/nextcloud-down.service"
 sudo sh -c "echo \"$UP_CONTENTS\" >> /etc/systemd/system/nextcloud-up.service"
+sudo systemctl enable check-mount.service
 sudo systemctl enable nextcloud-down.service
 sudo systemctl enable nextcloud-up.service
 
