5e1781f6e5aece12f2b0a607be74129437a039cd
ethmth <ethan@smaze.net>
2024-06-08 20:49:19 -0400

    Added memories app script

--- files/vm-resources/ubuntu/nextcloud/04-app/03-memories-app.sh
+++ files/vm-resources/ubuntu/nextcloud/04-app/03-memories-app.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "TODO"
+# memories
+VERSION="android/1.12"
+REPO="pulsejet/memories"
+
+RELEASE_ID=$(curl -s "https://api.github.com/repos/$REPO/releases/tags/$VERSION" | jq -r '.id')
+
+if [ "$RELEASE_ID" == "null" ]; then
+  echo "Release not found or authentication failed"
+  exit 1
+fi
+
+ASSET_URL=$(curl -s "https://api.github.com/repos/$REPO/releases/$RELEASE_ID/assets" | jq -r '.[] | select(.name | endswith(".apk")) | .browser_download_url')
+
+if [ -z "$ASSET_URL" ]; then
+  echo "No file found in the release assets"
+  exit 1
+fi
+
+curl -L -o memories.apk "$ASSET_URL"
