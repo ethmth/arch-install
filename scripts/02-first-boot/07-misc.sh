fb318a9cb832804db247d8271c1ae72cc43e2fb6
ethmth <ethan@smaze.net>
2024-06-15 22:55:53 -0400

    Build split-monitor-workspaces by manually copying header file

--- scripts/02-first-boot/07-misc.sh
+++ scripts/02-first-boot/07-misc.sh
@@ -164,24 +164,30 @@ if (( HYPRLAND )); then
         echo "Note: Delete /opt/hyprland/split-monitor-workspaces/split-monitor-workspaces.so to rebuild."
     else
         sudo mkdir -p /opt/hyprland
-        sudo chmod -R 777 /opt/hyprland
-        # if [ -d "/opt/hyprland/hyprland" ]; then
-        #     rm -rf /opt/hyprland/hyprland
-        # fi
-        # git clone --recurse-submodules https://github.com/hyprwm/Hyprland.git /opt/hyprland/hyprland
-        # cd /opt/hyprland/hyprland
-        #git checkout v0.40.0
+        #sudo chmod -R 777 /opt/hyprland
+        #if [ -d "/opt/hyprland/hyprland" ]; then
+        #    rm -rf /opt/hyprland/hyprland
+        #fi
+        #git clone --recurse-submodules https://github.com/hyprwm/Hyprland.git /opt/hyprland/hyprland
+        #cd /opt/hyprland/hyprland
+        #GIT_HYPRLAND_VERSION="v$(yay -Q hyprland | cut -d ' ' -f 2 | cut -d '-' -f 1)"
+        #git checkout $GIT_HYPRLAND_VERSION
         # make all
+        
+        if [ -d "/opt/hyprland/wlr" ]; then
+            rm -rf /opt/hyprland/wlr
+        fi
+        cp -r /usr/include/wlr /opt/hyprland
+        cp /usr/include/hyprland/wlroots-hyprland/wlr/util/transform.h /opt/hyprland/wlr/util/transform.h
 
         if [ -d "/opt/hyprland/split-monitor-workspaces" ]; then
             rm -rf /opt/hyprland/split-monitor-workspaces
         fi
         git clone https://github.com/Duckonaut/split-monitor-workspaces.git /opt/hyprland/split-monitor-workspaces
         cd /opt/hyprland/split-monitor-workspaces
-        #git checkout b40147d96d62a9e9bbc56b18ea421211ee598357
-        #export HYPRLAND_HEADERS="/opt/hyprland/hyprland"
-        #INCLUDE_PATH_LINE="COMPILE_FLAGS+=-I/opt/hyprland"
-        #sed -i "/COMPILE_FLAGS+=/a $INCLUDE_PATH_LINE" Makefile
+        export HYPRLAND_HEADERS="/opt/hyprland"
+        INCLUDE_PATH_LINE="COMPILE_FLAGS+=-I/opt/hyprland"
+        sed -i "/COMPILE_FLAGS+=/a $INCLUDE_PATH_LINE" Makefile
         make all
     fi
 fi
