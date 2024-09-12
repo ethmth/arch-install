599a48422188d3bc2185f4310f03069297d31d3e
ethmth <ethan@smaze.net>
2024-09-11 23:05:56 -0400

    Updated btrfs crypttab script

--- util/disk/btrfs/02-add-disk.sh
+++ util/disk/btrfs/02-add-disk.sh
@@ -43,7 +43,7 @@ chmod u=rw,g=,o= $path$name
 
 cryptsetup luksAddKey "$partition" "$path$name"
 
-options="luks"
+options="luks,x-systemd.device-timeout=20s"
 if (( SSD )); then
     options+=",discard"
 fi
