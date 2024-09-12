63a712dc4978b4978d277eef430de5068425d0b6
ethmth <ethan@smaze.net>
2024-09-11 23:40:03 -0400

    Updated btrfs automount to get uuid

--- util/disk/btrfs/03-automount.sh
+++ util/disk/btrfs/03-automount.sh
@@ -8,15 +8,17 @@ fi
 
 name=$(ls -1 /dev/mapper | grep -v "control" | fzf --prompt="Please select the /dev/mapper device you want to add an fstab entry for.")
 
+uuid=$(btrfs filesystem show "/dev/mapper/$name" | grep -o "uuid.*" | cut -d':' -f2 | tr -d ' ')
+
 read -p "Please the name for the mountpoint: " mountname
 
-read -p "Are you sure you want to mount /dev/mapper/$name to /mnt/$mountname (YES for yes, otherwise No)? " userInput
+read -p "Are you sure you want to mount /dev/mapper/$name (UUID $uuid) to /mnt/$mountname (YES for yes, otherwise No)? " userInput
 
 if ! [ "$userInput" == "YES" ]; then
     echo "Cancelling. No damage done."
     exit 1
 fi
 
-printf "/dev/mapper/$name\t/mnt/$mountname\tbtrfs\trw,async,nodev,nosuid,noexec,noatime,auto\t0 0\n" >> /etc/fstab
+printf "UUID=$uuid\t/mnt/$mountname\tbtrfs\trw,async,nodev,nosuid,noexec,noatime,auto\t0 0\n" >> /etc/fstab
 
 echo "Reboot and /dev/mapper/$name should be mounted automatically at /mnt/$mountname"
