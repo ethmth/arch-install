704e6b18a198cc8ed578eee359db810eaa0887b6
ethmth <ethan@smaze.net>
2024-11-07 00:52:35 -0500

    Fixed qemu downgrade script

--- util/misc/downgrade-qemu.sh
+++ util/misc/downgrade-qemu.sh
@@ -7,87 +7,16 @@ fi
 
 VERSION="9.1.0-2"
 
-pacman -U file:///var/cache/pacman/pkg/qemu-*$VERSION-x86_64.pkg.tar.zst
+packages=$(ls -1 /var/cache/pacman/pkg/qemu-* | grep "$VERSION-x86_64.pkg.tar.zst" | grep -v ".sig" | awk '{print "file://"$0}')
 
-exit 0
-
-packages="qemu-audio-alsa
-qemu-audio-dbus
-qemu-audio-jack
-qemu-audio-oss
-qemu-audio-pa
-qemu-audio-pipewire
-qemu-audio-sdl
-qemu-audio-spice
-qemu-base
-qemu-block-curl
-qemu-block-dmg
-qemu-block-nfs
-qemu-block-ssh
-qemu-chardev-spice
-qemu-common
-qemu-desktop
-qemu-hw-display-qxl
-qemu-hw-display-virtio-gpu
-qemu-hw-display-virtio-gpu-gl
-qemu-hw-display-virtio-gpu-pci
-qemu-hw-display-virtio-gpu-pci-gl
-qemu-hw-display-virtio-vga
-qemu-hw-display-virtio-vga-gl
-qemu-hw-s390x-virtio-gpu-ccw
-qemu-hw-usb-host
-qemu-hw-usb-redirect
-qemu-hw-usb-smartcard
-qemu-img
-qemu-pr-helper
-qemu-system-x86
-qemu-system-x86-firmware
-qemu-tools
-qemu-ui-curses
-qemu-ui-dbus
-qemu-ui-egl-headless
-qemu-ui-gtk
-qemu-ui-opengl
-qemu-ui-sdl
-qemu-ui-spice-app
-qemu-ui-spice-core
-qemu-vhost-user-gpu
-"
-
-packages+="qemu-block-gluster
-qemu-block-iscsi
-qemu-chardev-baum
-qemu-emulators-full
-qemu-full
-qemu-system-aarch64
-qemu-system-alpha
-qemu-system-arm
-qemu-system-avr
-qemu-system-cris
-qemu-system-hppa
-qemu-system-loongarch64
-qemu-system-m68k
-qemu-system-microblaze
-qemu-system-mips
-qemu-system-or1k
-qemu-system-ppc
-qemu-system-riscv
-qemu-system-rx
-qemu-system-s390x
-qemu-system-sh4
-qemu-system-sparc
-qemu-system-tricore
-qemu-system-xtensa
-qemu-tests
-qemu-user
-qemu-vmsr-helper"
+#pacman -U file:///var/cache/pacman/pkg/qemu-*$VERSION-x86_64.pkg.tar.zst
 
 
-awk_str="{print \$0\"-$VERSION-x86_64.pkg.tar.zst\"}"
-packages=$(echo "$packages" | awk '{print "file:///var/cache/pacman/pkg/"$0}' | awk "$awk_str")
+#awk_str="{print \$0\"-$VERSION-x86_64.pkg.tar.zst\"}"
+#packages=$(echo "$packages" | awk '{print "file:///var/cache/pacman/pkg/"$0}' | awk "$awk_str")
 
-packages=${packages//$'\n'/ }
-packages=$(echo "$packages" | tr -s ' ' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+#packages=${packages//$'\n'/ }
+#packages=$(echo "$packages" | tr -s ' ' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
 #echo "$packages"
 
