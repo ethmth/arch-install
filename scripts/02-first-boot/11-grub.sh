cf5cf0481ff7f5b38dc7c4334d1006cff14bda3f
ethmth <ethan@smaze.net>
2024-11-18 15:44:13 -0500

    Added conditional for desktop

--- scripts/02-first-boot/11-grub.sh
+++ scripts/02-first-boot/11-grub.sh
@@ -15,7 +15,10 @@ sudo bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/default
 #sudo bash /home/$CUR_USER/arch-install/util/kernel/config-update.sh /etc/default/grub "GRUB_DISABLE_OS_PROBER=false"
 
 sudo bash /home/$CUR_USER/arch-install/util/kernel/grub-add.sh usbcore.autosuspend=-1
-sudo bash /home/$CUR_USER/arch-install/util/kernel/grub-add.sh pcie_aspm=off
+
+if (( DESKTOP )); then #unlimit ethernet
+    sudo bash /home/$CUR_USER/arch-install/util/kernel/grub-add.sh pcie_aspm=off
+fi
 
 if (( NVIDIA )); then
     sudo bash /home/$CUR_USER/arch-install/util/kernel/grub-add.sh nvidia_drm.modeset=1
