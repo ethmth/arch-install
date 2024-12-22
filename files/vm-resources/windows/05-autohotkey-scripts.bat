482110e4e666340cc56eed87f0c6467d1fcdaab5
ethmth <ethan@smaze.net>
2024-12-22 13:23:22 -0500

    Got a partially working ps script

--- files/vm-resources/windows/05-autohotkey-scripts.bat
+++ files/vm-resources/windows/05-autohotkey-scripts.bat
@@ -1,15 +1,26 @@
 @echo off
 set "StartupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 set "LocalPath=autohotkey"
-set "ScriptPath=$env:USERPROFILE\Documents\autohotkey"
+set "ScriptPath=%USERPROFILE%\Documents\autohotkey"
+set "ScriptFile=pushtotalk.ahk"
 set "ShortcutName=pushtotalk.lnk"
-
-powershell -Command "Copy-Item -Path '%LocalPath%' -Destination "%ScriptPath%" -Recurse"
-
-powershell -Command "$WshShell = New-Object -ComObject WScript.Shell; `
-    $Shortcut = $WshShell.CreateShortcut('%StartupFolder%\%ShortcutName%'); `
-    $Shortcut.TargetPath = '%ScriptPath%\pushtotalk.ahk'; `
-    $Shortcut.Save()"
-
-echo Shortcut created in the Startup folder.
-
+ 
+:: Ensure the directory exists before copying
+if not exist "%ScriptPath%" mkdir "%ScriptPath%"
+ 
+:: Copy the AutoHotkey script using PowerShell
+powershell -Command "Copy-Item -Path '%LocalPath%\%ScriptFile%' -Destination '%ScriptPath%'"
+ 
+:: Ensure the script exists before creating a shortcut
+if not exist "%ScriptPath%\%ScriptFile%" (
+            echo Script not found at "%ScriptPath%\%ScriptFile%". Aborting.
+                pause
+                    exit /b
+            )
+ 
+            :: Create shortcut using PowerShell (single-line)
+            powershell -Command "$WScriptShell = New-Object -ComObject WScript.Shell; $Shortcut = $WScriptShell.CreateShortcut('%StartupFolder%\\%ShortcutName%'); $Shortcut.TargetPath = '%ScriptPath%\\%ScriptFile%'; $Shortcut.WorkingDirectory = '%ScriptPath%'; $Shortcut.Arguments = ''; $Shortcut.Save();"
+ 
+            echo Shortcut created at %StartupFolder%\%ShortcutName%
+            pause
+ 
