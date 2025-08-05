f0571af9e687a1294ec9248ad0b50d00238e17e3
ethmth <ethan@smaze.net>
2025-08-04 20:59:14 -0400

    Added auto selection of devices

--- scripts/vm/02-evdev/01-evdev-devices.sh
+++ scripts/vm/02-evdev/01-evdev-devices.sh
@@ -7,30 +7,58 @@ fi
 
 # SELECT DEVICES
 
-DEVICE_DIR="/dev/input/by-id"
-
-DEVICES=$(ls -1 $DEVICE_DIR | fzf -m --prompt="Please select your devices")
-
+read -p "Do you want to auto-select devices? (y/N): " user_input
 
 DEVICE_LIST=""
-for device in $DEVICES; do
-        DEVICE_LIST+="$DEVICE_DIR/$device\n"
-done
-
-
-sudo mkdir -p /etc/libvirt/hooks-scripts
-printf "$DEVICE_LIST" > /etc/libvirt/hooks-scripts/evdev-devices.list
-
+DEVICE_DIR="/dev/input/by-id"
+if [[ $user_input == "y" ]] || [[ $user_input == "Y" ]]; then
+        for entry in `ls -l $DEVICE_DIR/ | grep "event*"`
+        do
+                KBD_M=`echo "$entry" | rev | cut -c -4 | rev`
+                if [ "$KBD_M" = "ouse" ]
+                        then
+                                DEVICE_LIST+="$DEVICE_DIR/$entry\n"
+                elif  [ "$KBD_M" = "-kbd" ]
+                then
+                                DEVICE_LIST+="$DEVICE_DIR/$entry\n"
+                fi
+        done
+else 
+        DEVICES=$(ls -1 $DEVICE_DIR | fzf -m --prompt="Please select your devices")
+        if [[ -z "$DEVICES" ]]; then
+                echo "No devices selected."
+                exit 1
+        fi
+        DEVICE_LIST=""
+        for device in $DEVICES; do
+                DEVICE_LIST+="$DEVICE_DIR/$device\n"
+        done
+fi
 
 # SELECT VIRTUAL MACHINES
 
 VMS=$(virsh list --all --name | fzf -m --prompt="Please select your virtual machines")
+if [[ -z "$VMS" ]]; then
+        echo "No virtual machines selected."
+        exit 1
+fi
 
 VM_LIST=""
 for vm in $VMS; do
         VM_LIST+="$vm\n"
 done
 
+printf "===================== DEVICE LIST =====================\n"
+printf "$DEVICE_LIST"
+printf "===================== DEVICE LIST =====================\n"
+
+printf "===================== VM LIST =====================\n"
+printf "$VM_LIST"
+printf "===================== VM LIST =====================\n"
+
+
+sudo mkdir -p /etc/libvirt/hooks-scripts
+printf "$DEVICE_LIST" > /etc/libvirt/hooks-scripts/evdev-devices.list
 printf "$VM_LIST" > /etc/libvirt/hooks-scripts/evdev-vms.list
 
 
