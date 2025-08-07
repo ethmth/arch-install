90c402a256c09c1232a1f758addcd67756f20c58
ethmth <ethan@smaze.net>
2025-08-07 19:58:27 -0400

    Updated mtu script

--- files/vm-resources/windows/00-set-mtu.bat
+++ files/vm-resources/windows/00-set-mtu.bat
@@ -1,7 +1,9 @@
 @echo off
 
 echo Open powershell admin and run
-echo netsh interface ipv4 set subinterface "Ethernet" mtu=1320 store=persistent
-echo Replacing 1320 with the MTU of your VPN interface, if less than the default of 1500; replace "Ethernet" with your network interface name
+echo "netsh interface ipv4 set subinterface "Ethernet" mtu=1420 store=persistent"
+echo Replacing 1420 with the MTU of your VPN interface, if less than the default of 1500; replace "Ethernet" with your network interface name
+
+echo Run "netsh interface ipv4 show subinterfaces" to confirm the change and view the interfaces
 
 pause
