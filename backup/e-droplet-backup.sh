3a701d8ccd190d36351540ccf5ab538c17d1c090
ethmth <ethan@smaze.net>
2026-05-10 20:48:28 -0400

    Added crontab and e-droplet-backup script

--- backup/e-droplet-backup.sh
+++ backup/e-droplet-backup.sh
@@ -5,3 +5,8 @@ echo 'e-droplet backup'
 echo `date -Iseconds`
 echo '*****'
 echo
+
+echo '*** Valvopes ***'
+ssh e-droplet docker container exec valvopes-db pg_dump -U postgres data > /mnt/cryptdata/MEGA/Personal/Software/Valvopes/backups/automated_backup.sql
+echo "Backup completed successfully."
+echo
