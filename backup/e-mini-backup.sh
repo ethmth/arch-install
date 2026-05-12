28c8292347bcd3c4e5ed1b9d173e0ff4d938eb62
ethmth <ethan@smaze.net>
2026-05-11 20:59:13 -0400

    Switched to e-mini-local

--- backup/e-mini-backup.sh
+++ backup/e-mini-backup.sh
@@ -7,5 +7,5 @@ echo '*****'
 echo
 
 echo '*** gitea-repo-backup ***'
-rsync -az --info=stats2 --mkpath e-mini:/home/e/docker/gitea-repo-backup/backups/ /mnt/cryptdata/MEGA/Personal/GithubBackups/GiteaBackups
+rsync -az --info=stats2 --mkpath e-mini-local:/home/e/docker/gitea-repo-backup/backups/ /mnt/cryptdata/MEGA/Personal/GithubBackups/GiteaBackups
 echo
